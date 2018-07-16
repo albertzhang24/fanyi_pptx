cdff18b3dda8104be960e97175e43076f07bdcfd
Albert Zhang <albertzhang@Albert-MacBook-Pro.local>
2018-07-16 14:34:24 -0700

    should be finalized

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import json
 import sys, os
 from hashlib import md5
 import sys, hashlib, random, json, string
-#import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.parse, urllib.error
 import http.client
 
 import requests
@@ -17,7 +17,8 @@ appid = '20180531000169614'
 secretKey = 'IRP4F_z0fxRscAU5aMt_'
 salt = random.randint(32768, 65536)
 
-BUCKET_NAME = 'test-http-post'
+
+BUCKET_NAME = 'fanyi-pptx'
 
 
 def home(event, context):
@@ -37,42 +38,43 @@ def home(event, context):
 
 def translate(event, context):
     # We are expecting the user to send us an url.
-    untranslated_pptx_url = event['url']
+    untranslated_pptx_url = json.loads(event['body'])['url']
 
     # Download the package and write to tmp location.
     tmp_path = os.path.join('/tmp', os.path.basename(untranslated_pptx_url))
     untranslated_pptx_response = requests.get(untranslated_pptx_url, stream=True)
 
     with open(tmp_path, 'wb') as tmp_path_file:
-        for block in response.iter_content(1024):
-            tmp_path_file.write(untranslated_pptx_response)
+        for block in untranslated_pptx_response.iter_content(1024):
+            tmp_path_file.write(block)
 
     # Translate the PPTX and upload to S3 converted bucket.
-    translated_filepath = translate_powerpoint(untranslated_pptx_response)
+    translated_filepath = translate_powerpoint(tmp_path)
     translated_url = upload_to_s3(translated_filepath)
-
+    print(translated_url)
     # Return the converted S3 URL.
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
         },
-        'body': {
-            url: translated_url
-        }
+        'body': json.dumps({
+            'url': translated_url
+        })
     }
 
 
 def translate_powerpoint(input_filename):
-    httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-
-    source_target = request.form['source_target']
-    if source_target[:source_target.find("_")] == 'en':
-        sourceLang = 'en'
-        targetLang = 'zh'
-    else:
-        sourceLang = 'zh'
-        targetLang = 'en'
+    # source_target = request.form['source_target']
+    sourceLang = 'en'
+    targetLang = 'zh'
+    
+    # if source_target[:source_target.find("_")] == 'en':
+    #     sourceLang = 'en'
+    #     targetLang = 'zh'
+    # else:
+    #     sourceLang = 'zh'
+    #     targetLang = 'en'
 
     source_ppt_str = input_filename
     ppt = Presentation(input_filename)
@@ -85,14 +87,16 @@ def translate_powerpoint(input_filename):
             if shape.has_text_frame:
                 for paragraph in shape.text_frame.paragraphs:
                     for run in paragraph.runs:
-                        run.text = _translate(run.text)
+                        run.text = _translate(run.text, sourceLang, targetLang)
 
     ppt.save(target_ppt)
 
     return target_ppt
 
 
-def _translate(q):
+def _translate(q, sourceLang, targetLang):
+    httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+
     m1 = hashlib.md5()
     m1.update((appid+q+str(salt)+secretKey).encode('utf-8'))
     sign = m1.hexdigest()
@@ -124,13 +128,14 @@ def _translate(q):
 
 def upload_to_s3(filepath):
     client = boto3.client('cognito-identity')
-    client.get_credentials_for_identity(
-        IdentityId='us-west-2:f0b0917c-128b-4ab6-809b-833ce5e82597',
+    credentials = client.get_credentials_for_identity(
+        IdentityId='us-west-2:7e280494-3998-488e-832e-14bf8d4abde7',
     )
 
     s3 = boto3.resource('s3',
-        aws_access_key_id=client['Credentials']['AccessKeyId'],
-        aws_secret_access_key=client['Credentials']['SecretKey'],
+        aws_access_key_id=credentials['Credentials']['AccessKeyId'],
+        aws_secret_access_key=credentials['Credentials']['SecretKey'],
+        aws_session_token=credentials['Credentials']['SessionToken'],
         config=Config(signature_version='s3v4')
     )
 
